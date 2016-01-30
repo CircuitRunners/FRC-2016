00f3424fa378c7285b4fb9df6374fa537305ccb5
mastercoms <circuitsoft@tuta.io>
2016-01-30 14:12:13 -0500

    Clean up code

--- src/com/github/circuitrunners/Robot.java
+++ src/com/github/circuitrunners/Robot.java
@@ -4,39 +4,31 @@ package com.github.circuitrunners;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import java.lang.reflect.Constructor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Robot extends IterativeRobot {
 
-    RobotDrive drive;
+    private RobotDrive drive;
+    private Joystick joystick;
+    private AnalogGyro gyro;
 
-    VictorSP motor;
-
-    Joystick joystick;
-
-    AnalogGyro gyro;
+    private static final ExecutorService parallelExecutor = Executors.newCachedThreadPool();
+    private static final ExecutorService sequentialExecutor = Executors.newSingleThreadExecutor();
 
     @Override
     public void robotInit() {
-
-        drive = new RobotDrive(5,4,1,0);
+        drive = new RobotDrive(5, 4, 1, 0);
         drive.setExpiration(0.5);
-
-        motor = new VictorSP(3);
 
         joystick = new Joystick(0);
 
         gyro = new AnalogGyro(0);
         gyro.calibrate();
-
     }
 
     @Override
     public void autonomousInit() {
-        ExecutorService parallelExecutor = Executors.newCachedThreadPool();
-        ExecutorService sequentialExecutor = Executors.newSingleThreadExecutor();
         sequentialExecutor.execute(new AutonomousDriveThread(1, 0, 1000));
         sequentialExecutor.execute(new AutonomousDriveThread(-1, 0, 1000));
     }
@@ -72,90 +64,65 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void autonomousPeriodic() {
-
     }
 
     @Override
     public void teleopInit() {
-        SmartDashboard.putNumber("derp", 0);
     }
 
     @Override
     public void teleopPeriodic() {
-        drive(joystick);
-
+        drive(joystick.getY(), joystick.getX(), true);
     }
 
-    public void drive(double moveVal, double rotateVal) {
-//        double gyroVal = gyro.getAngle();
-//
-//        if (gyroVal != Math.asin(rotateVal)) {
-//            if (gyroVal < 357 && gyroVal > 183) {
-//                rotateVal -= Math.sin(gyroVal);
-//            } else if (gyroVal > 3 && gyroVal < 177) {
-//                rotateVal += Math.sin(gyroVal);
-//            }
-//        }
-
+    private void drive(double moveVal, double rotateVal) {
         drive.arcadeDrive(moveVal, rotateVal);
-    }
-
-    private void drive(Joystick joystick) {
-
-        double moveVal = -joystick.getY(); // wtf are directions
-        double twistVal = -joystick.getTwist(); //jesus cant save you now
-        double throttleVal = -joystick.getThrottle(); //why is everything negative?
-
-        double rotateVal = scalePower(twistVal, 0.1, 0.7, 2); //could make magic numbers into constants but who cares
-        double throttledMove = throttleMath(throttleVal) * moveVal; //0% chance we need this elsewhere but who cares
-
-        drive.arcadeDrive(throttledMove, rotateVal);
 
         SmartDashboard.putNumber("moveVal", moveVal);
         SmartDashboard.putNumber("rotateVal", rotateVal);
         SmartDashboard.putNumber("gyro", gyro.getAngle());
+    }
 
-        motor.set(SmartDashboard.getNumber("derp",0));
+    private void drive(double moveVal, double rotateVal, boolean input) {
+        if (input) {
+            double throttleVal = -joystick.getThrottle();
+
+            moveVal = throttleMath(throttleVal) * moveVal;
+            rotateVal = scalePower(rotateVal, 0.1, 0.7, 2);
+        }
+
+        drive(moveVal, rotateVal);
     }
 
     private double scaleLinear(double input, double min, double scale) {
         double output = 0;
-        if(Math.abs(input) < min) {
-            output = 0;
-        }
-        else {
+        if (Math.abs(input) >= min) {
             output = scale * input;
         }
         return output;
     }
 
-    private double scaleDoubleFlat(double input, double min, double scale, double power){
-        double output = 0;
-        if(Math.abs(input) < min) {
+    private double scaleDoubleFlat(double input, double min, double scale, double power) {
+        double output;
+        if (Math.abs(input) < min) {
             output = 0;
-        }
-        else if(Math.abs(input) > scale){
+        } else if (Math.abs(input) > scale) {
             output = Math.signum(input) * scale;
-        }
-        else{
+        } else {
             output = scale * Math.signum(input) * Math.abs(Math.pow(input/scale,power));
         }
         return output;
     }
 
-    private double scalePower(double input, double min, double scale, double power){
+    private double scalePower(double input, double min, double scale, double power) {
         double output = 0;
-        if(Math.abs(input) < min) {
-            output = 0;
-        }
-        else {
+        if (Math.abs(input) >= min) {
             output = scale * Math.signum(input) * Math.abs(Math.pow(input,power));
         }
         return output;
     }
 
-    private double throttleMath(double input){
-        double output = (input+1)/2;
-        return output;
+    private double throttleMath(double input) {
+        return (input + 1) / 2;
     }
 }
