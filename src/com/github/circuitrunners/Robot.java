1ce94510ff36681c583b3e6eba630466560c9cd9
akilan <akilan98@gmail.com>
2016-01-30 10:04:16 -0500

    Leave out gyro temp

--- src/com/github/circuitrunners/Robot.java
+++ src/com/github/circuitrunners/Robot.java
@@ -1,8 +1,14 @@
 
 package com.github.circuitrunners;
 
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+import java.lang.reflect.Constructor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Robot extends IterativeRobot {
 
@@ -10,30 +16,50 @@ public class Robot extends IterativeRobot {
 
     AnalogGyro gyro;
 
-    VictorSP motors[];
-
-    /* no pid for now
-    ArrayList<PIDController> pids;
-    */
+    RobotDrive drive;
 
     @Override
     public void robotInit() {
         joystick = new Joystick(0);
         gyro = new AnalogGyro(0);
-        motors = new VictorSP[]{new VictorSP(0), new VictorSP(3), new VictorSP(4), new VictorSP(5)};
-        /* no pid for now
-        pids = new ArrayList<>();
-        for (CANTalon motor : motors) {
-            pids.add(new PIDController(1, 1, 1, 1, gyro, motor));
-        }
-        */
-        motors[0].setInverted(true);
-        motors[1].setInverted(true);
+        drive = new RobotDrive(0, 1, 2, 3);
     }
-
+    
     @Override
     public void autonomousInit() {
+        ExecutorService parallelExecutor = Executors.newCachedThreadPool();
+        ExecutorService sequentialExecutor = Executors.newSingleThreadExecutor();
+        sequentialExecutor.execute(new AutonomousDriveThread(1, 0, 1000));
+        sequentialExecutor.execute(new AutonomousDriveThread(-1, 0, 1000));
+    }
 
+    private class AutonomousDriveThread implements Runnable {
+        double moveVal;
+        double rotateVal;
+        int waitTime;
+
+        public AutonomousDriveThread(double moveVal, double rotateVal) {
+            this.moveVal = moveVal;
+            this.rotateVal = rotateVal;
+        }
+
+        public AutonomousDriveThread(double moveVal, double rotateVal, int waitTime) {
+            this.moveVal = moveVal;
+            this.rotateVal = rotateVal;
+            this.waitTime = waitTime;
+        }
+
+        @Override
+        public void run() {
+            drive(moveVal, rotateVal);
+            if (waitTime > 0) {
+                try {
+                    Thread.sleep(waitTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
@@ -48,39 +74,21 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
-        double rotateVal = joystick.getTwist();
-        if (joystick.getRawButton(1)) {
-            gyro.reset();
-        }
+        drive(joystick.getY(), joystick.getX());
+    }
 
-        if (0 <= rotateVal && rotateVal < 90) {
-            motors[0].set(Math.sin(rotateVal));
-            motors[1].set(Math.sin(rotateVal));
-            motors[2].set(joystick.getMagnitude());
-            motors[3].set(joystick.getMagnitude());
-        } else if (90 <= rotateVal && rotateVal < 180) {
-            motors[2].set(Math.sin(rotateVal));
-            motors[3].set(Math.sin(rotateVal));
-            motors[0].set(joystick.getMagnitude());
-            motors[1].set(joystick.getMagnitude());
-        } else if (180 <= rotateVal && rotateVal < 270){
-            motors[2].set(Math.sin(rotateVal));
-            motors[3].set(Math.sin(rotateVal));
-            motors[0].set(-joystick.getMagnitude());
-            motors[1].set(-joystick.getMagnitude());
-        } else if (270 <= rotateVal && rotateVal < 360){
-            motors[0].set(Math.sin(rotateVal));
-            motors[1].set(Math.sin(rotateVal));
-            motors[2].set(-joystick.getMagnitude());
-            motors[3].set(-joystick.getMagnitude());
-        } else {
-            for (VictorSP motor : motors) {
-                motor.set(0);
-            }
-        }
-        System.out.println("\n");
-        SmartDashboard.putNumber("rotateVal", rotateVal);
-        SmartDashboard.putData("test", gyro);
+    public void drive(double moveVal, double rotateVal) {
+//        double gyroVal = gyro.getAngle();
+//
+//        if (gyroVal != Math.asin(rotateVal)) {
+//            if (gyroVal < 357 && gyroVal > 183) {
+//                rotateVal -= Math.sin(gyroVal);
+//            } else if (gyroVal > 3 && gyroVal < 177) {
+//                rotateVal += Math.sin(gyroVal);
+//            }
+//        }
+
+        drive.arcadeDrive(moveVal, rotateVal);
     }
 
     @Override
