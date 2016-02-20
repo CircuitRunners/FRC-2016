db28c9f90327747c17ea4154e289ec22db7947c1
mastercoms <circuitsoft@openmailbox.org>
2016-02-19 19:45:07 -0500

    added vision stuff

--- src/main/java/com/github/circuitrunners/Robot.java
+++ src/main/java/com/github/circuitrunners/Robot.java
@@ -1,11 +1,16 @@
 
 package com.github.circuitrunners;
 
-import com.akilib.SmartDashboard2;
 import com.analog.adis16448.frc.ADIS16448_IMU;
+import com.github.circuitrunners.akilib.SmartDashboard2;
+import com.github.circuitrunners.akilib.Xbox;
 import com.github.circuitrunners.calib.CalibMath;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.tables.ITable;
+import edu.wpi.first.wpilibj.vision.AxisCamera;
+
+import java.util.Random;
 
 public class Robot extends IterativeRobot {
 
@@ -19,6 +24,7 @@ public class Robot extends IterativeRobot {
     private static final int PORT_SHOOTER_RIGHT = 5;
     private static final int PORT_SHOOTER_LIFT = 0;
     private static final int PORT_SHOOTER_KICKER = 1;
+    private static final int PORT_JOYSTICK = 0;
 
     // Drive Adjustments
     private static final double JOYSTICK_DEADZONE = 0.1;
@@ -26,8 +32,17 @@ public class Robot extends IterativeRobot {
     private static final double JOYSTICK_SCALE_POWER = 1;
     private static final double JOYSTICK_DEADZONE_PID = 0.2;
     private static final double DISABLEDPIDLIFTSPEEDMULTIPLIERCALLMEKYLE = 0.3;
-    private static final double SPEED_SHOOTER_KICKER_OUT = 0.5;
-    private static final double SPEED_SHOOTER_KICKER_IN = 0.1;
+    private static final String DRIVER_CONTROL_TYPE = "VERSION_1";
+
+    // Camera Constants
+    private static final double CAMERA_RESOLUTION_Y = 480;
+    private static final double CAMERA_RESOLUTION_X = 640;
+    private static final double TOP_TARGET_HEIGHT = 97;
+    private static final double VERTICAL_FOV = 51.4;
+    private static final double HORIZONTAL_FOV = 67;
+    private static final double CAMERA_ANGLE = 13;
+    private static final double TOP_CAMERA_HEIGHT = 14;
+
 
     // Axes
     private static int AXIS_MOVE = 1;
@@ -36,12 +51,14 @@ public class Robot extends IterativeRobot {
 
     // Buttons
     private static int BUTTON_PID_ENABLE = 4;
-    private static int BUTTON_GYRO_RESET = 5;
+    private static int BUTTON_GYRO_RESET = 6;
 
-    private static int BUTTON_SHOOTER_WHEELSPIN_OUT = 1;
-    private static int BUTTON_SHOOTER_WHEELSPIN_IN = 2;
     private static int BUTTON_SHOOTER_LIFT_DOWN = 3;
     private static int BUTTON_SHOOTER_LIFT_UP = 5;
+    private static int BUTTON_SHOOTER_WHEELSPIN_OUT = 1;
+    private static int BUTTON_SHOOTER_WHEELSPIN_IN = 2;
+    private static final double SPEED_SHOOTER_KICKER_OUT = 1;
+    private static final double SPEED_SHOOTER_KICKER_IN = 0;
 
     //PID Constants
     private static final double KP_POT = 0.002;
@@ -61,7 +78,7 @@ public class Robot extends IterativeRobot {
     private static final double SPEED_SHOOTER_WHEEL_LEFT = 1;
     private static final double SPEED_SHOOTER_WHEEL_RIGHT = 1;
 
-    private static final double ANGLE_LIFT_DEFAULT = 60;
+    private static final double ANGLE_LIFT_DEFAULT = 30;
     private static final double TOLERANCE_PID_POT = 0.1;
 //    private static final double OFFSET_SHOOTER = 0.17;
 //    private static final double SPEED_SHOOTER_LIFT_UP = -0.5;
@@ -101,8 +118,11 @@ public class Robot extends IterativeRobot {
     private double thatAdjustment;
     private double theOtherAdjustment;
     private PIDController potPID;
-    private double[] imageValues;
     private double targetAngle;
+
+    private AxisCamera camera;
+
+    private final NetworkTable grip = NetworkTable.getTable("GRIP");
 
     @Override
     public void robotInit() {
@@ -114,7 +134,7 @@ public class Robot extends IterativeRobot {
 
         drive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
 
-        drive.setExpiration(0.75);
+        drive.setExpiration(5);
 
         shooterWheelLeft = new VictorSP(PORT_SHOOTER_LEFT);
         shooterWheelRight = new VictorSP(PORT_SHOOTER_RIGHT);
@@ -127,7 +147,7 @@ public class Robot extends IterativeRobot {
         potPID.setPercentTolerance(TOLERANCE_PID_POT);
         potPID.enable();
 
-        joystick = new Joystick(0);
+        joystick = new Joystick(PORT_JOYSTICK);
         
         thisShit = new ADIS16448_IMU();
         thisShit.calibrate();
@@ -140,6 +160,8 @@ public class Robot extends IterativeRobot {
         theOtherShit = new AnalogGyro(0);
         theOtherShit.calibrate();
         theOtherPIDController = new PIDController(KP_THE_OTHER_SHIT, 0, KD_THE_OTHER_SHIT, theOtherShit, output -> {});
+
+        camera = new AxisCamera("10.10.2.11");
     }
 
     @Override
@@ -165,7 +187,8 @@ public class Robot extends IterativeRobot {
         weatherStatus = CalibMath.answerQuestion();
 
         // Drive Controls
-        if (SmartDashboard2.get("isXbox", joystick.getIsXbox())) {
+        if (joystick.getIsXbox()) {
+            joystick = new Xbox(PORT_JOYSTICK);
             AXIS_MOVE = 1;
             AXIS_ROTATE = 4;
 
@@ -174,19 +197,21 @@ public class Robot extends IterativeRobot {
 
             BUTTON_SHOOTER_LIFT_DOWN = 1;
             BUTTON_SHOOTER_LIFT_UP = 2;
-            BUTTON_SHOOTER_WHEELSPIN_OUT = 6;
             BUTTON_SHOOTER_WHEELSPIN_IN = 5;
+            BUTTON_SHOOTER_WHEELSPIN_OUT = 6;
+            SmartDashboard2.put("isXbox", true);
+        } else {
+            SmartDashboard2.put("isXbox", false);
+            setControlType(SmartDashboard2.get("driverControlType", DRIVER_CONTROL_TYPE));
         }
+
         targetAngle = SmartDashboard2.get("targetAngle", ANGLE_LIFT_DEFAULT);
-        NetworkTable.getTable("SmartDashboard").globalDeleteAll();
-        NetworkTable.getTable("SmartDashboard").flush();
     }
 
     boolean triggerPressed; //so lonely
 
     @Override
     public void teleopPeriodic() {
-
         drive();
         liftShooter();
         shootAndIntake();
@@ -205,10 +230,18 @@ public class Robot extends IterativeRobot {
         SmartDashboard2.put("Will it rain?", weatherStatus);
 
         SmartDashboard2.put("Hall", shooterKickerReturnSensor);
+        SmartDashboard2.put("pot", pot);
         SmartDashboard2.put("PotPID", potPID);
-        boolean isShooterAimed = SmartDashboard2.get("distance", 99999) < 500 &&
-                                 SmartDashboard2.get("azimuth", 99999) < 50;
-        SmartDashboard2.put("goodToGo", isShooterAimed);
+       // SmartDashboard2.put("goodToGo", isShooterAimed);
+        SmartDashboard2.put("distanceTraveledX", thisShit.getMagX());
+        SmartDashboard2.put("distanceTraveledY", thisShit.getMagY());
+        SmartDashboard2.put("distanceTraveledZ", thisShit.getMagZ());
+
+        double[] temp = getStuff();
+        if (temp != null) {
+            SmartDashboard2.put("azimuth", temp[1]);
+            SmartDashboard2.put("distance", temp[0]);
+        }
     }
 
     private void drive() {
@@ -245,8 +278,8 @@ public class Robot extends IterativeRobot {
         }
 
         //Smart Gyro™
-        SmartDashboard2.put("SmartGyro", CalibMath.average(CalibMath.gyroLimit(thatAdjusted),
-                                                           CalibMath.gyroLimit(theOtherAdjusted)));
+        SmartDashboard2.put("SmartGyro", CalibMath.average(CalibMath.normalize360(thatAdjusted),
+                                                           CalibMath.normalize360(theOtherAdjusted)));
 
         double throttledRotate = SmartDashboard2.put("throttledRotate",
                                                      CalibMath.inverseAdjustedDeadband(throttleVal,0.5) * rotateVal
@@ -292,7 +325,7 @@ public class Robot extends IterativeRobot {
             if ((pot.get()-1950)/17.4 < 420-300) SmartDashboard2.put("targetAngle", ++targetAngle);
             isDirectionUp = 1;
         } else if (joystick.getRawButton(BUTTON_SHOOTER_LIFT_DOWN)) {
-            if ((pot.get()-1950)/17.4 < 420-300 && SmartDashboard2.get("targetAngle",0) > 33) {
+            if ((pot.get()-1950)/17.4 < 420-300 && SmartDashboard2.get("targetAngle",0) > 30) {
                 SmartDashboard2.put("targetAngle", --targetAngle);
             }
             isDirectionUp = -1;
@@ -315,24 +348,76 @@ public class Robot extends IterativeRobot {
         // Shooter Wheels
         if (joystick.getRawButton(BUTTON_SHOOTER_WHEELSPIN_IN)) {
             if (shooterKickerReturnSensor.get()) shooterKicker.set(SPEED_SHOOTER_KICKER_IN);
-            Timer.delay(0.1);
             shooterWheelLeft.set(shooterLeftWheelSpeed);
             shooterWheelRight.set(-shooterRightWheelSpeed);
         } else if (joystick.getRawButton(BUTTON_SHOOTER_WHEELSPIN_OUT)) {
             shooterWheelLeft.set(-shooterLeftWheelSpeed);
             shooterWheelRight.set(shooterRightWheelSpeed);
-            Timer.delay(0.5); // Spinning up...
             shooterKicker.set(-SPEED_SHOOTER_KICKER_OUT);
         } else {
             shooterWheelLeft.set(0);
             shooterWheelRight.set(0);
-            if (shooterKickerReturnSensor.get()) shooterKicker.set(SPEED_SHOOTER_KICKER_IN);
-            else shooterKicker.set(0);
+            shooterKicker.set(0);
+        }
+    }
+
+    public double[] getStuff(){
+        final ITable table = grip.getSubTable("contours");
+        for (String key : table.getKeys()) {
+            if (table.getNumberArray(key, new double[0]).length == 0) {
+                System.out.println("No contours for you!" + new Random().nextBoolean());
+                return null;
+            }
+        }
+
+        int largest = CalibMath.getLargestIndex(table.getNumberArray("area",new double[0]));
+
+        double[] centerXs = table.getNumberArray("centerX", new double[0]);
+        double centerX = centerXs[largest];
+
+        double[] centerYs = table.getNumberArray("centerY", new double[0]);
+        double centerY = centerYs[largest];
+
+        double[] widths = table.getNumberArray("width", new double[0]);
+        double width = widths[largest];
+
+        double[] heights = table.getNumberArray("height", new double[0]);
+        double height = heights[largest];
+
+        double[] areas = table.getNumberArray("area", new double[0]);
+        double area = areas[largest];
+
+        double y= -((2 * (centerY / CAMERA_RESOLUTION_Y)) - 1);
+        double distance = (TOP_TARGET_HEIGHT - TOP_CAMERA_HEIGHT) /
+                Math.tan((y * VERTICAL_FOV / 2.0 + CAMERA_ANGLE) * Math.PI / 180);
+//				angle to target...would not rely on this
+        double targetX = (2 * (centerX / CAMERA_RESOLUTION_X)) - 1;
+        double azimuth = CalibMath.normalize360(targetX*HORIZONTAL_FOV /2.0 + 0);
+
+        double[] values = {distance,azimuth};
+        return values;
+    }
+
+    private void moveDistance(double distance) {
+        double initialX = thisShit.getMagX();
+        double initialY = thisShit.getMagY();
+        while (thisShit.getMagX() < initialX + distance) {
+            drive.arcadeDrive(1, 0);
+        }
+    }
+
+
+    private void setControlType(String driverControlType) {
+        switch (driverControlType) {
+            case "VERSION_2":
+                break;
+            case "VERSION_3":
+                break;
         }
     }
 
     @Override
     public void disabledInit() {
-        System.out.println("Default IterativeRobot.disabledInit() method... Overload me, Chandler!");
+        System.out.println("Not default IterativeRobot.disabledInit() method... Overload me, Chandler!");
     }
 }
